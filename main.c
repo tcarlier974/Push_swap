d5285f51b5cbf0fb42e14c6a100cd60b50e1b4fa
tcarlier <carliertiti97459@gmail.com>
2025-02-25 23:51:13 +0100

    menage

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: tcarlier <tcarlier@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/23 17:26:13 by tcarlier          #+#    #+#             */
-/*   Updated: 2025/02/25 23:26:19 by tcarlier         ###   ########.fr       */
+/*   Updated: 2025/02/25 23:51:08 by tcarlier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,7 +41,7 @@ int	main(int ac, char **av)
 	else
 	{
 		args = av;
-		ac--;
+		ac = ac - 2;
 	}
 	if (!parse_args(ac, args, &data))
 	{
